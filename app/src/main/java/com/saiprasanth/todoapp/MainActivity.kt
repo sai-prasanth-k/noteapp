6a588626acb788247ca93c53b49876ce8588d3c9
sai-prasanth-k <saiprasanth5293@gmail.com>
2021-12-13 07:45:13 +0530

    done a simple todo app using kotlin

---
+++ app/src/main/java/com/saiprasanth/todoapp/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.saiprasanth.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.SparseBooleanArray
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var itemlist = arrayListOf<String>()
+        var adapter =ArrayAdapter<String>(this,
+            android.R.layout.simple_list_item_multiple_choice
+            , itemlist)
+        val add = findViewById<Button>(R.id.add)
+        val listView = findViewById<ListView>(R.id.listView)
+        val clear = findViewById<Button>(R.id.clear)
+        val delete = findViewById<Button>(R.id.delete)
+        val editText = findViewById<TextView>(R.id.editText)
+
+
+        add.setOnClickListener {
+
+            itemlist.add(editText.text.toString())
+            listView.adapter =  adapter
+            adapter.notifyDataSetChanged()
+
+            editText.text.clear()
+        }
+
+        clear.setOnClickListener {
+
+            itemlist.clear()
+            adapter.notifyDataSetChanged()
+        }
+
+        listView.setOnItemClickListener { adapterView, view, i, l ->
+            android.widget.Toast.makeText(this, "You Selected the item --> "+itemlist.get(i), android.widget.Toast.LENGTH_SHORT).show()
+        }
+
+        delete.setOnClickListener {
+            val position: SparseBooleanArray = listView.checkedItemPositions
+            val count = listView.count
+            var item = count - 1
+            while (item >= 0) {
+                if (position.get(item))
+                {
+                    adapter.remove(itemlist.get(item))
+                }
+                item--
+            }
+            position.clear()
+            adapter.notifyDataSetChanged()
+        }
+    }
+}
+
+private fun CharSequence.clear() {
+
+}
